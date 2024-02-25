425d2b63fc3cb4089be95c0cad27f703d0bf4f69
DimaPuz2010 <91826731+DimaPuz2010@users.noreply.github.com>
2024-02-25 12:57:29 +0600

    car and company

---
+++ flaskr/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    @app.route("/")
+    def display_main_page():
+        return render_template("index.html")
+    
+    @app.route("/cars")
+    def display_cars_company():
+        return render_template("cars_company.html")
+    
+
+    return app
